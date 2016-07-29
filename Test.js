415d8aa237fc74ef5185264690c9d253dad25ab6
somedude101 <supercolin25@gmail.com>
2016-07-29 11:00:36 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,1 +1,7 @@
-//test//
+Game.UpdateMenu=function()
+		{
+			if (Game.onMenu=='prefs')
+			{
+				Game.WriteButton('test', 'test', 'on', 'off', 'Game.ToggleFancy();')+'<label>(visual improvements; disabling may improve performance)</label><br>'+
+			}
+		}
