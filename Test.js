e530e79f158f0a7cfad1e165ce56ff75cf192273
somedude101 <supercolin25@gmail.com>
2016-08-03 10:55:42 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,4 +1,3 @@
 if (Game.onMenu=='stats')
 {
-  Game.WriteButton('fancy','fancyButton','test 1','test 2','Game.ToggleFancy();')+'<label>(visual improvements; disabling may improve performance)</label><br>'+
-}
+  Game.WriteButton('fancy','fancyButton','test 1','test 2','Game.ToggleFancy();')+'<label>(visual improvements; disabling may improve performance)</label><br>'
