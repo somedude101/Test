cd5ad7282fca9bc2a5d8c2f2c59bc2fde1fd4444
somedude101 <supercolin25@gmail.com>
2016-08-04 08:13:00 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,1 +1,20 @@
+Addon.AddMenu = function()
+{
+  var title = function()
+  {
+		var div = document.createElement('div');
+		div.className = 'title ' + Addon.colorTextPre + Addon.colorBlue;
+		div.textContent = 'Addon Options';
+		return div;
+	}
+	if (Game.onMenu == 'prefs')
+		Addon.AddMenuPref(title);
+	else if (Addon.Stats == 1 && Game.onMenu == 'stats')
+		Addon.AddMenuStats(title);
+}
 
+Addon.RefreshMenu = function()
+{
+	if (Game.onMenu == 'stats')
+	  Game.UpdateMenu();
+}
