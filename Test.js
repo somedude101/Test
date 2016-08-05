55aa4242a2541b0a08ff3db7e5e515d7bf3144ec
somedude101 <supercolin25@gmail.com>
2016-08-05 12:26:21 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -65,7 +65,7 @@ Addon.Cache.AddQueue = function()
 {
 	Addon.Cache.Queue = document.createElement('script');
 	Addon.Cache.Queue.type = 'text/javascript';
-	Addon.Cache.Queue.setAttribute('src', 'Addon.Oueue()');
+	Addon.Cache.Queue.setAttribute('src', 'Addon.Queue()');
 	document.head.appendChild(Addon.Cache.Queue);
 }
 
