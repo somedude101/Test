b8070bced69a6cef841b38f3c271e2412f7f7683
somedude101 <supercolin25@gmail.com>
2016-08-07 14:12:02 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -2,14 +2,14 @@ Addon = {};
 
 Addon.AddMenu = function()
 {
-	var str2 = '';
+	var str = '';
 	if (Game.onMenu == 'prefs')
 	{
-		str2 += '<div class="subsection">'+
+		str += '<div class="subsection">'+
 		'<div class="title">Addon</div>'+
 		'<div style="padding-bottom:128px;"></div>'
 	}
-	l('menu').innerHTML = str2;
+	l('menu').innerHTML += str;
 }
 Addon.CCMenu = Game.UpdateMenu;
 Game.UpdateMenu = function()
