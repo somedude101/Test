c4d5416b87e8fd56e8ae1a771c8bef9df76ce4ab
somedude101 <supercolin25@gmail.com>
2016-08-07 18:34:45 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,28 +1,189 @@
 Addon = {};
 
+Addon.CCPrefs = Game.DefaultPrefs;
 Game.DefaultPrefs=function()
 {
-	Game.prefs.particles=1;
-	Game.prefs.numbers=1;
-	Game.prefs.autosave=1;
-	Game.prefs.autoupdate=1;
-	Game.prefs.milk=1;
-	Game.prefs.fancy=1;
-	Game.prefs.warn=0;
-	Game.prefs.cursors=1;
-	Game.prefs.focus=1;
-	Game.prefs.popups=0;
-	Game.prefs.format=0;
-	Game.prefs.notifs=0;
-	Game.prefs.animate=1;
-	Game.prefs.wobbly=1;
-	Game.prefs.monospace=0;
-	Game.prefs.filters=0;
-	Game.prefs.cookiesound=1;
-	Game.prefs.crates=0;
-	Game.prefs.altDraw=0;
+	Addon.CCPrefs();
 	Game.prefs.statistics = 0;
 }
+
+Game.WriteSave=function(type)
+		{
+			//type : none is default, 1=return string only, 2=return uncompressed string, 3=return uncompressed, commented string
+			Game.lastDate=parseInt(Date.now());
+			var str='';
+			if (type==3) str+='\nGame version\n';
+			str+=Game.version+'|';
+			str+='|';//just in case we need some more stuff here
+			if (type==3) str+='\n\nRun details';
+			str+=//save stats
+			(type==3?'\n	run start date : ':'')+parseInt(Game.startDate)+';'+
+			(type==3?'\n	legacy start date : ':'')+parseInt(Game.fullDate)+';'+
+			(type==3?'\n	date when we last opened the game : ':'')+parseInt(Game.lastDate)+';'+
+			(type==3?'\n	bakery name : ':'')+(Game.bakeryName)+
+			'|';
+			if (type==3) str+='\n\nPacked preferences bitfield\n	';
+			var str2=//prefs
+			(Game.prefs.particles?'1':'0')+
+			(Game.prefs.numbers?'1':'0')+
+			(Game.prefs.autosave?'1':'0')+
+			(Game.prefs.autoupdate?'1':'0')+
+			(Game.prefs.milk?'1':'0')+
+			(Game.prefs.fancy?'1':'0')+
+			(Game.prefs.warn?'1':'0')+
+			(Game.prefs.cursors?'1':'0')+
+			(Game.prefs.focus?'1':'0')+
+			(Game.prefs.format?'1':'0')+
+			(Game.prefs.notifs?'1':'0')+
+			(Game.prefs.wobbly?'1':'0')+
+			(Game.prefs.monospace?'1':'0')+
+			(Game.prefs.filters?'1':'0')+
+			(Game.prefs.cookiesound?'1':'0')+
+			(Game.prefs.crates?'1':'0')+
+			(Game.prefs.statistics?'1':'0')+
+			'';
+			str2=pack2(str2);
+			str+=str2+'|';
+			if (type==3) str+='\n\nMisc game data';
+			str+=
+			(type==3?'\n	cookies : ':'')+parseFloat(Game.cookies).toString()+';'+
+			(type==3?'\n	total cookies earned : ':'')+parseFloat(Game.cookiesEarned).toString()+';'+
+			(type==3?'\n	cookie clicks : ':'')+parseInt(Math.floor(Game.cookieClicks))+';'+
+			(type==3?'\n	golden cookie clicks : ':'')+parseInt(Math.floor(Game.goldenClicks))+';'+
+			(type==3?'\n	cookies made by clicking : ':'')+parseFloat(Game.handmadeCookies).toString()+';'+
+			(type==3?'\n	golden cookies missed : ':'')+parseInt(Math.floor(Game.missedGoldenClicks))+';'+
+			(type==3?'\n	background type : ':'')+parseInt(Math.floor(Game.backgroundType))+';'+
+			(type==3?'\n	milk type : ':'')+parseInt(Math.floor(Game.milkType))+';'+
+			(type==3?'\n	cookies from past runs : ':'')+parseFloat(Game.cookiesReset).toString()+';'+
+			(type==3?'\n	elder wrath : ':'')+parseInt(Math.floor(Game.elderWrath))+';'+
+			(type==3?'\n	pledges : ':'')+parseInt(Math.floor(Game.pledges))+';'+
+			(type==3?'\n	pledge time left : ':'')+parseInt(Math.floor(Game.pledgeT))+';'+
+			(type==3?'\n	currently researching : ':'')+parseInt(Math.floor(Game.nextResearch))+';'+
+			(type==3?'\n	research time left : ':'')+parseInt(Math.floor(Game.researchT))+';'+
+			(type==3?'\n	ascensions : ':'')+parseInt(Math.floor(Game.resets))+';'+
+			(type==3?'\n	golden cookie clicks (this run) : ':'')+parseInt(Math.floor(Game.goldenClicksLocal))+';'+
+			(type==3?'\n	cookies sucked by wrinklers : ':'')+parseFloat(Game.cookiesSucked).toString()+';'+
+			(type==3?'\n	wrinkles popped : ':'')+parseInt(Math.floor(Game.wrinklersPopped))+';'+
+			(type==3?'\n	santa level : ':'')+parseInt(Math.floor(Game.santaLevel))+';'+
+			(type==3?'\n	reindeer clicked : ':'')+parseInt(Math.floor(Game.reindeerClicked))+';'+
+			(type==3?'\n	season time left : ':'')+parseInt(Math.floor(Game.seasonT))+';'+
+			(type==3?'\n	season switcher uses : ':'')+parseInt(Math.floor(Game.seasonUses))+';'+
+			(type==3?'\n	current season : ':'')+(Game.season?Game.season:'')+';';
+			var wrinklers=Game.SaveWrinklers();
+			str+=
+			(type==3?'\n	amount of cookies contained in wrinklers : ':'')+parseFloat(Math.floor(wrinklers.amount))+';'+
+			(type==3?'\n	number of wrinklers : ':'')+parseInt(Math.floor(wrinklers.number))+';'+
+			(type==3?'\n	prestige level : ':'')+parseFloat(Game.prestige).toString()+';'+
+			(type==3?'\n	heavenly chips : ':'')+parseFloat(Game.heavenlyChips).toString()+';'+
+			(type==3?'\n	heavenly chips spent : ':'')+parseFloat(Game.heavenlyChipsSpent).toString()+';'+
+			(type==3?'\n	heavenly cookies : ':'')+parseFloat(Game.heavenlyCookies).toString()+';'+
+			(type==3?'\n	ascension mode : ':'')+parseInt(Math.floor(Game.ascensionMode))+';'+
+			(type==3?'\n	permanent upgrades : ':'')+parseInt(Math.floor(Game.permanentUpgrades[0]))+';'+parseInt(Math.floor(Game.permanentUpgrades[1]))+';'+parseInt(Math.floor(Game.permanentUpgrades[2]))+';'+parseInt(Math.floor(Game.permanentUpgrades[3]))+';'+parseInt(Math.floor(Game.permanentUpgrades[4]))+';'+
+			(type==3?'\n	dragon level : ':'')+parseInt(Math.floor(Game.dragonLevel))+';'+
+			(type==3?'\n	dragon aura : ':'')+parseInt(Math.floor(Game.dragonAura))+';'+
+			(type==3?'\n	dragon aura 2 : ':'')+parseInt(Math.floor(Game.dragonAura2))+';'+
+			(type==3?'\n	chime type : ':'')+parseInt(Math.floor(Game.chimeType))+';'+
+			(type==3?'\n	volume : ':'')+parseInt(Math.floor(Game.volume))+';'+
+			(type==3?'\n	number of shiny wrinklers : ':'')+parseInt(Math.floor(wrinklers.shinies))+';'+
+			(type==3?'\n	amount of cookies contained in shiny wrinklers : ':'')+parseFloat(Math.floor(wrinklers.amountShinies))+';'+
+			'|';//cookies and lots of other stuff
+			
+			if (type==3) str+='\n\nBuildings : amount, bought, cookies produced, special unlocked';
+			for (var i in Game.Objects)//buildings
+			{
+				var me=Game.Objects[i];
+				if (type==3) str+='\n	'+me.name+' : ';
+				if (me.vanilla) str+=me.amount+','+me.bought+','+parseFloat(Math.floor(me.totalCookies))+','+(me.specialUnlocked?1:0)+';';
+			}
+			str+='|';
+			if (type==3) str+='\n\nPacked upgrades bitfield (unlocked and bought)\n	';
+			var toCompress=[];
+			for (var i in Game.UpgradesById)//upgrades
+			{
+				var me=Game.UpgradesById[i];
+				if (me.vanilla) toCompress.push(Math.min(me.unlocked,1),Math.min(me.bought,1));
+			};
+			
+			toCompress=pack2(toCompress.join(''));//toCompress=pack(toCompress);//CompressLargeBin(toCompress);
+			
+			str+=toCompress;
+			str+='|';
+			if (type==3) str+='\n\nPacked achievements bitfield (won)\n	';
+			var toCompress=[];
+			for (var i in Game.AchievementsById)//achievements
+			{
+				var me=Game.AchievementsById[i];
+				if (me.vanilla) toCompress.push(Math.min(me.won));
+			}
+			toCompress=pack2(toCompress.join(''));//toCompress=pack(toCompress);//CompressLargeBin(toCompress);
+			str+=toCompress;
+			
+			if (type==3) str+='\n';
+			
+			for (var i in Game.customSave) {Game.customSave[i]();}
+			
+			if (type==2 || type==3)
+			{
+				return str;
+			}
+			else if (type==1)
+			{
+				str=escape(utf8_to_b64(str)+'!END!');
+				return str;
+			}
+			else
+			{
+				if (Game.useLocalStorage)
+				{
+					//so we used to save the game using browser cookies, which was just really neat considering the game's name
+					//we're using localstorage now, which is more efficient but not as cool
+					//a moment of silence for our fallen puns
+					str=utf8_to_b64(str)+'!END!';
+					if (str.length<10)
+					{
+						if (Game.prefs.popups) Game.Popup('Error while saving.<br>Purchasing an upgrade might fix this.');
+						else Game.Notify('Saving failed!','Purchasing an upgrade and saving again might fix this.<br>This really shouldn\'t happen; please notify Orteil on his tumblr.');
+					}
+					else
+					{
+						str=escape(str);
+						window.localStorage.setItem(Game.SaveTo,str);//aaand save
+						if (!window.localStorage.getItem(Game.SaveTo))
+						{
+							if (Game.prefs.popups) Game.Popup('Error while saving.<br>Export your save instead!');
+							else Game.Notify('Error while saving','Export your save instead!');
+						}
+						else if (document.hasFocus())
+						{
+							if (Game.prefs.popups) Game.Popup('Game saved');
+							else Game.Notify('Game saved','','',1,1);
+						}
+					}
+				}
+				else//legacy system
+				{
+					//that's right
+					//we're using cookies
+					//yeah I went there
+					var now=new Date();//we storin dis for 5 years, people
+					now.setFullYear(now.getFullYear()+5);//mmh stale cookies
+					str=utf8_to_b64(str)+'!END!';
+					Game.saveData=escape(str);
+					str=Game.SaveTo+'='+escape(str)+'; expires='+now.toUTCString()+';';
+					document.cookie=str;//aaand save
+					if (document.cookie.indexOf(Game.SaveTo)<0)
+					{
+						if (Game.prefs.popups) Game.Popup('Error while saving.<br>Export your save instead!');
+						else Game.Notify('Error while saving','Export your save instead!','',0,1);
+					}
+					else if (document.hasFocus())
+					{
+						if (Game.prefs.popups) Game.Popup('Game saved');
+						else Game.Notify('Game saved','','',1,1);
+					}
+				}
+			}
+		}
 
 Addon.AddMenu = function()
 {
